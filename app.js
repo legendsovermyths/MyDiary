e1977812e5e50e480931397523db569cb26025eb
Mask Wolf <maskwolf2000@gmail.com>
2020-07-18 22:06:14 +0530

    add database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,44 +11,64 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/post', {useNewUrlParser: true, useUnifiedTopology: true});
-app.set('view engine', 'ejs');
-const postsSchema={
-  title:String,
-  content:String
-}
-const Post=mongoose.model("post",postsSchema)
-var posts=[];
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.get("/",function(req,res){
-  res.render("home",{text1:homeStartingContent,posts:posts})
-})
-app.get("/post/:topic",function(req,res){
-  for(var i=0;i<posts.length;i++){
-    if (_.lowerCase(posts[i].postTitle)==_.lowerCase(req.params.topic)){
-      res.render("post",{postTitle:posts[i].postTitle,postContent:posts[i].postContent})
-    }
-  }
-})
-app.get("/about",function(req,res){
-  res.render("about",{text2:aboutContent})
-})
-app.get("/contact",function(req,res){
-  res.render("contact",{text3:contactContent})
-})
-app.get("/compose",function(req,res){
-  res.render("compose")
-})
-app.post("/compose",function(req,res){
-  var postParts={
-    postTitle:req.body.postTitle,
-    postContent:req.body.postContent
-  }
-  posts.push(postParts)
-  res.redirect("/")
-})
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+mongoose.connect('mongodb://localhost:27017/postDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+app.set('view engine', 'ejs');
+const postsSchema = {
+  title: String,
+  content: String
+}
+const Post = mongoose.model("post", postsSchema)
+var posts = [];
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+  Post.find({}, function(err, post) {
+    console.log(post);
+    res.render("home", {
+      text1: homeStartingContent,
+      posts: post
+    })
+  })
+
+})
+app.get("/post/:topic", function(req, res) {
+      idOfPost = req.params.topic
+      Post.findOne({
+          _id: idOfPost
+        }, function(err, foundPost) {
+          res.render("post", {
+              postTitle: foundPost.title,
+              postContent: foundPost.content});
+          })
+      })
+      app.get("/about", function(req, res) {
+      res.render("about", {
+        text2: aboutContent
+      })
+    })
+    app.get("/contact", function(req, res) {
+      res.render("contact", {
+        text3: contactContent
+      })
+    })
+    app.get("/compose", function(req, res) {
+      res.render("compose")
+    })
+    app.post("/compose", function(req, res) {
+      const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postContent,
+      })
+      post.save()
+      console.log(req.body)
+      res.redirect("/")
+    })
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+    });
