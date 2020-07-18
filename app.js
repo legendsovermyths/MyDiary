0825e7c9e9ed444f5d9e5836caec0b10f96ea17b
Mask Wolf <maskwolf2000@gmail.com>
2020-07-18 23:29:06 +0530

    added a procfile

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const ejs = require("ejs");
 const mongoose = require("mongoose")
 const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hi! This web application is currently in its beta stage and uses MongoDB on backend. This application has a hidden page through which you can compose multiple posts which will be displayed on the home page. To access the compose page, you will be required to type '/compose' after the URL. Feel free to use the application. ";
+const aboutContent = "Hi! My name is Anirudh Singh and I am currently a student of Bits Pilani, Hyderabad campus. I am an aspiring web and app developer. You can check out my work on my git, on the following link\:\
+'https://github.com/legendsovermyths'";
+const contactContent = "You can contact my following social media accounts\:gmail\:\ anirudh.anirudhsingh.singh@gmail.com instagram\:\ https\:\www.instagram.com\/\anibitte facebook\:\ https:www.facebook.com\/\anirudh.anirudhsingh.singh"
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/postDB', {
+mongoose.connect('mongodb+srv://Admin:test123@cluster0.jio15.mongodb.net/postDB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -29,7 +30,6 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.get("/", function(req, res) {
   Post.find({}, function(err, post) {
-    console.log(post);
     res.render("home", {
       text1: homeStartingContent,
       posts: post
@@ -53,9 +53,7 @@ app.get("/post/:topic", function(req, res) {
       })
     })
     app.get("/contact", function(req, res) {
-      res.render("contact", {
-        text3: contactContent
-      })
+      res.render("contact", )
     })
     app.get("/compose", function(req, res) {
       res.render("compose")
@@ -69,6 +67,11 @@ app.get("/post/:topic", function(req, res) {
       console.log(req.body)
       res.redirect("/")
     })
-    app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+    app.listen(port, function() {
       console.log("Server started on port 3000");
     });
